c1c2dbad67c15214ae0c4bbbf3493bd9fd95b725
Tom <tom@ubuntu.(none)>
2012-09-10 22:43:49 +0300

    mysite commit

---
+++ mysite/old_urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('polls.views',
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^mysite/', include('mysite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^polls/$', 'index'),
+    url(r'^polls/(?P<poll_id>\d+)/$', 'detail'),
+    url(r'^polls/(?P<poll_id>\d+)/results/$', 'results'),
+    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'vote'),
+)
+# Uncomment the next line to enable the admin: 
+urlpatterns +=patterns('',
+    url(r'^admin/', include(admin.site.urls)), 
+)
---
+++ polls/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import datetime
+from django.utils import timezone
+from django.template import RequestContext
+
+# Create your models here.
+class Poll(models.Model):
+    question = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    def __unicode__(self):
+	return self.question
+    def was_published_recently(self):
+	return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+class Choice(models.Model):
+    poll = models.ForeignKey(Poll)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField()
+    def __unicode__(self):
+	return self.choice_text
+
+def detail(request, poll_id):
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p},
+                               context_instance=RequestContext(request))
